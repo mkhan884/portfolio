722d7932aa5f4cde4696756abfa388bdc5627248
Muhammad Khan <mkwestern1@gmail.com>
2024-10-14 20:53:04 +0400

    please work

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -13,7 +13,11 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@fortawesome/fontawesome-svg-core': '/node_modules/@fortawesome/fontawesome-svg-core/index.js',
+      '@fortawesome/free-solid-svg-icons': '/node_modules/@fortawesome/free-solid-svg-icons/index.js',
+      '@fortawesome/free-brands-svg-icons': '/node_modules/@fortawesome/free-brands-svg-icons/index.js',
+      '@fortawesome/vue-fontawesome': '/node_modules/@fortawesome/vue-fontawesome/index.js'
     },
-  },
-});
+    },
+  });
