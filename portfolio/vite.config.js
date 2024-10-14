5d21ee0bff47907712ed6d78be3d8a5bd181f3bf
Muhammad Khan <mkwestern1@gmail.com>
2024-10-14 20:49:59 +0400

    TEST FIRST

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -8,17 +8,7 @@ export default defineConfig({
   ],
   build: {
     rollupOptions: {
-      external:
-        [
-            '@fortawesome/fontawesome-svg-core',
-            '@fortawesome/free-brands-svg-icons',
-            '@fortawesome/free-solid-svg-icons',
-            '@fortawesome/vue-fontawesome',
-            'vuetify',
-            'vuetify/components',
-            'vuetify/directives',
-            'vuetify/styles',
-          ]
+      external: []
     },
   },
   resolve: {
