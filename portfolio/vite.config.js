788b483c11bd13f17a00565be66042d7a230eb8c
Muhammad Khan <mkwestern1@gmail.com>
2024-10-14 18:58:07 +0400

    Dixing configuration

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -8,6 +8,11 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  build: {
+    rollupOptions: {
+      external: ["@fortawesome/fontawesome-svg-core", '@fortawesome/free-brands-svg-icons', '@fortawesome/free-brands-svg-icons', '@fortawesome/free-solid-svg-icons'], // list all external packages here
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
