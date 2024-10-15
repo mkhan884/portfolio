2a318352ba827fb7545ca9687925eaa6cabca483
Muhammad Khan <mkwestern1@gmail.com>
2024-10-15 10:21:22 +0400

    updating config file

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -15,5 +15,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+    dedupe: ['@fortawesome/free-brands-svg-icons', '@fortawesome/fontawesome-svg-core', '@fortawesome/free-solid-svg-icons', '@fortawesome/vue-fontawesome'],  // This helps Vite resolve the package correctly
     },
   });
